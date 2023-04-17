17ff7df4761665ccc2d012603f0e64a0ac5ad159
santos <info@santos.main.jp>
2023-04-17 22:23:07 +0900

    first commit

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,36 @@
+class QuestionsController < ApplicationController
+  # 質問一覧ページ表示
+  def index
+
+  end
+
+  # 質問詳細ページの表示
+  def show
+
+  end
+
+  # 質問の作成
+  def new
+    @question = Question.new
+  end
+
+  # 質問の登録
+  def create
+    p params
+  end
+
+  # 質問の編集
+  def edit
+
+  end
+
+  # 質問の更新
+  def update
+
+  end
+
+  # 質問の削除
+  def destroy
+
+  end
+end
