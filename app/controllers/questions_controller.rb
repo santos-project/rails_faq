95952b136ac22c57dc2239ecb8719a12d8499420
santos <info@santos.main.jp>
2023-04-18 22:26:14 +0900

    compile

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -22,24 +22,34 @@ class QuestionsController < ApplicationController
     # Questionモデルを初期化
     @question = Question.new(question_params)
     # QuestionモデルをDBへ保存
-    @question.save
+    if @question.save
     # showへリダイレクト
-    redirect_to @question
+      redirect_to @question
+    else
+      render 'new', status: :unprocessable_entity
+    end
   end
 
   # 質問の編集
   def edit
-
+    @question = Question.find(params[:id])
   end
 
   # 質問の更新
   def update
-
+    @question =Question.find(params[:id])
+    if @question.update(question_params)
+      redirect_to @question
+    else
+      render 'edit', status: :unprocessable_entity
+    end
   end
 
   # 質問の削除
   def destroy
-
+    @question = Question.find(params[:id])
+    @question.destroy
+    redirect_to questions_path
   end
 
   private
